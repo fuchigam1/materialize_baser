2a0e586d1b33f31efecae631c3b35a7a921cb45a
arata <contact@materializing.net>
2015-09-27 22:08:37 +0900

    add テーマ用ファイル

--- js/init.js
+++ js/init.js
@@ -3,8 +3,10 @@
 		$('.button-collapse').sideNav();
 		$('.parallax').parallax();
 
+		/* 画像ポップアップ用 */
 		$("a[rel='colorbox']").colorbox({transition:"fade"});
 
+		/* メールフォームの input 調整用 */
 		$("input[type=radio]").each(function() {
 			$inputHtml = $(this);
 			$(this).parent().before($inputHtml);
@@ -16,7 +18,13 @@
 		$("select").each(function() {
 			$(this).wrap('<div class="input-field">');
 		});
+		$("textarea").each(function() {
+			$(this).wrap('<div class="input-field">');
+		});
+		/* http://amsul.ca/pickadate.js/date/#formatting-rules */
 		$('.datepicker').pickadate({
+			format: 'yyyy-mm-dd',
+			formatSubmit: 'yyyy/mm/dd',
 			selectMonths: true, // Creates a dropdown to control month
 			selectYears: 15 // Creates a dropdown of 15 years to control year
 		});
